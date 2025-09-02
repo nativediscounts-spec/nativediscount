e3dcd6fc0b6cf3ba05bf09e6894f97473f18a43c
MyDigiLink <146575379+mydigilink@users.noreply.github.com>
2025-09-02 16:00:49 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="robots" content="noindex, nofollow" />
+      </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
