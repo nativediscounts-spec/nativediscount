928dae1905c07685e82d9f7c2340e191ee224bdb
MyDigiLink <146575379+mydigilink@users.noreply.github.com>
2025-09-02 16:01:13 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-   test          Get started by editing <code>src/app/page.js</code>.
+   File tested          Get started by editing <code>src/app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
