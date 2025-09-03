80003c95c1aef61ad4da806b2ba20408ffdfdaa6
MyDigiLink <146575379+mydigilink@users.noreply.github.com>
2025-09-03 22:43:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "/api/uploads/:path*", // serve uploads via API
+      },
+    ];
+  },
+  // experimental options go here if supported in your version
+};
 
 export default nextConfig;
