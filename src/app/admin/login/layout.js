edc166f739ed1ea48486129e932e59708a9f35e4
MyDigiLink <146575379+mydigilink@users.noreply.github.com>
2025-10-01 01:04:39 +0530

    Update layout.js

--- src/app/admin/login/layout.js
+++ src/app/admin/login/layout.js
@@ -44,7 +44,7 @@ export default function LoginLayout({ children }) {
   };
 
   return (
-    <div className="container vh-100 d-flex align-items-center justify-content-center bg-light">
+    <div className="container vh-100 d-flex align-items-center justify-content-center ">
       <div className="card shadow-sm p-4" style={{ width: "350px" }}>
         <h3 className="text-center mb-4">Admin Login</h3>
         {error && <div className="alert alert-danger py-2">{error}</div>}
