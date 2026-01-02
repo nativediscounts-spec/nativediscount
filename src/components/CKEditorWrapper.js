0d483c209cee089a6801c8181a663fe5bada034f
MyDigiLink <146575379+mydigilink@users.noreply.github.com>
2026-01-02 17:33:19 +0530

    Update CKEditorWrapper.js

--- src/components/CKEditorWrapper.js
+++ src/components/CKEditorWrapper.js
@@ -1,7 +1,17 @@
 "use client";
 
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import { CKEditor } from "@ckeditor/ckeditor5-react";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import dynamic from "next/dynamic";
+const CKEditor = dynamic(
+  () => import("@ckeditor/ckeditor5-react").then(mod => mod.CKEditor),
+  { ssr: true }
+);
+
+const ClassicEditor = dynamic(
+  () => import("@ckeditor/ckeditor5-build-classic").then(mod => mod.default),
+  { ssr: true }
+);
 
 export default function CKEditorWrapper({ value, onChange }) {
   return (
